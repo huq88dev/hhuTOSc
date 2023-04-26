3d355f697aeeecb5ef9cb37f09e5ebfcfb4cd729
Lian Remme <lirem101@hhu.de>
2023-04-26 15:43:14 +0200

    Changed allocator to BumpAllocator

--- src/kernel/Globals.h
+++ src/kernel/Globals.h
@@ -14,7 +14,7 @@
 #include "devices/PCSPK.h"
 #include "devices/CGA_Stream.h"
 #include "kernel/allocator/BumpAllocator.h"
-#include "kernel/allocator/LinkedListAllocator.h"
+//#include "kernel/allocator/LinkedListAllocator.h"
 #include "devices/Keyboard.h"
 
 extern CPU              cpu;        // CPU-spezifische Funktionen
@@ -22,7 +22,7 @@ extern CGA_Stream       kout;       // Ausgabe-Strom fuer Kernel
 extern Keyboard         kb;
 extern PCSPK            pcspk;      // PC-Lautsprecher
 extern uint64_t         total_mem;  // RAM total
-//extern BumpAllocator    allocator;
-extern LinkedListAllocator   allocator;
+extern BumpAllocator    allocator;
+//extern LinkedListAllocator   allocator;
 
 #endif
